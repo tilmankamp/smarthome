029fcb092731e72313821e80ed3d19ef06aec52e
Kelly Davis <kdavis@mozilla.com>
2016-03-17 10:42:25 +0100

    Issue #1162: Implementation of Keyword Spotting API
    Signed-off-by: Kelly Davis <kdavis@mozilla.com>

---
+++ bundles/io/org.eclipse.smarthome.io.voice/src/main/java/org/eclipse/smarthome/io/voice/KeywordSpottingErrorEvent.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2014-2016 openHAB UG (haftungsbeschraenkt) and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.smarthome.io.voice;
+
+/**
+ * A {@link KSEvent} fired when the {@link KSService} encounters an error.
+ *
+ * @author Kelly Davis - Initial contribution and API
+ */
+public class KeywordSpottingErrorEvent implements KSEvent {
+   /**
+    * The message describing the error
+    */
+    private final String message;
+
+   /**
+    * Constructs an instance with the passed {@code message}.
+    *
+    * @param message The message describing the error
+    */
+    public KeywordSpottingErrorEvent(String message) {
+        this.message = message;
+    }
+
+   /**
+    * Gets the message describing this error
+    *
+    * @return The message describing this error
+    */
+    public String getMessage() {
+        return this.message;
+    }
+}
