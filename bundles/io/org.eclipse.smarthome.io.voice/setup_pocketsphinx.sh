302389a0b1127866b072c856419ad1ce1066810c
Tilman Kamp <tilmankamp@posteo.de>
2016-04-20 16:35:56 +0200

    Moved pocketsphinx compilation to voice package
    Signed-off-by: Tilman Kamp <tilmankamp@posteo.de>

--- bundles/io/org.eclipse.smarthome.io.voice/setup_pocketsphinx.sh
+++ bundles/io/org.eclipse.smarthome.io.voice/setup_pocketsphinx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+root=`pwd`
 set -e
 
 echo "##### This script installs pocketsphinx libraries for Vaani-Iot."
@@ -9,12 +9,11 @@ git submodule update
 cd pocketsphinx/sphinxbase
 git pull origin master
 ./autogen.sh
-./configure --prefix=`pwd`/../../bundles/io/org.eclipse.smarthome.io.voice/lib/ps/ && make && make install 
+./configure --prefix=${root}/lib/ps/ && make && make install 
 cd ../pocketsphinx
 git pull origin master
 ./autogen.sh
-./configure --prefix=`pwd`/../../bundles/io/org.eclipse.smarthome.io.voice/lib/ps/ && make && make install 
-
+./configure --prefix=${root}/lib/ps/ && make && make install 
 
 echo "##### Generating swig libraries...."
 cd swig
@@ -28,8 +27,6 @@ echo "##### Generating the maven-nar artifact."
 mvn clean install
 
 echo "##### Setting the pocketsphinx model folder into esh properties."
-directory=`pwd`
-echo "modelpath = $directory/../../../../pocketsphinx/pocketsphinx/model/en-us/" > "$directory/../../../../bundles/io/org.eclipse.smarthome.io.voice/src/main/resources/pocketsphinx_en_US.properties"
-
+echo "modelpath = ${root}/pocketsphinx/pocketsphinx/model/en-us/" > "${root}/src/main/resources/pocketsphinx_en_US.properties"
 
 echo "##### Done! Now do a full maven build of ESH."
