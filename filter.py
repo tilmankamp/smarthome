d9c3fc08496e174bf8964c9abf0c4b85caa59d83
Tilman Kamp <tilmankamp@posteo.de>
2016-05-23 10:08:45 +0200

    Travis configuration
    Signed-off-by: Tilman Kamp <tilmankamp@posteo.de>

---
+++ filter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import fileinput
+
+mode = 0
+counter = 0
+
+for line in fileinput.input():
+	if line.startswith("[INFO] Reactor Summary:"):
+		print
+		print
+		mode = 1
+	if mode == 1:
+		sys.stdout.write(line)
+	else:
+		if line.startswith("[INFO] Building") and "jar:" not in line and "zip:" not in line:
+			print
+			sys.stdout.write(line.replace("\n", " ").replace("\r", ""))
+			sys.stdout.flush()
+			counter = 0
+		else:
+			counter = (counter + 1) % 10
+			if counter == 0:
+				sys.stdout.write(".")
+	sys.stdout.flush()
+print
